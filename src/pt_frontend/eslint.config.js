580609103331fec5de10369d6e0e96724fffa564
Daniël Terwiel <daniel@terwiel.io>
2024-12-15 13:41:37 +0100

    chore: improve eslint (#11)
    * chore: ignore declarations on lint
    
    * chore: eslint-xstate-plugin

--- src/pt_frontend/eslint.config.js
+++ src/pt_frontend/eslint.config.js
@@ -2,17 +2,26 @@ import pluginJs from '@eslint/js';
 import pluginBiome from 'eslint-config-biome';
 import pluginPerfectionist from 'eslint-plugin-perfectionist';
 import pluginReact from 'eslint-plugin-react';
+import pluginXState from 'eslint-plugin-xstate';
 import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['**/node_modules/**', '**/dist/**', '**/src/components/ui/**'],
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/src/components/ui/**',
+      '**/declarations/**',
+    ],
   },
   {
     settings: {
       react: {
         version: 'detect',
+      },
+      xstate: {
+        version: 5,
       },
     },
   },
@@ -33,6 +42,32 @@ export default [
   pluginReact.configs.flat.recommended,
   pluginPerfectionist.configs['recommended-natural'],
   pluginBiome,
+  {
+    plugins: {
+      xstate: pluginXState,
+    },
+    rules: {
+      'xstate/entry-exit-action': 'error',
+      'xstate/event-names': ['warn', 'macroCase'],
+      'xstate/invoke-usage': 'error',
+      'xstate/no-async-guard': 'error',
+      'xstate/no-auto-forward': 'warn',
+      'xstate/no-imperative-action': 'error',
+      'xstate/no-infinite-loop': 'error',
+      'xstate/no-inline-implementation': 'warn',
+      'xstate/no-invalid-conditional-action': 'error',
+      'xstate/no-invalid-state-props': 'error',
+      'xstate/no-invalid-transition-props': 'error',
+      'xstate/no-misplaced-on-transition': 'error',
+      'xstate/no-ondone-outside-compound-state': 'error',
+      'xstate/prefer-always': 'error',
+      'xstate/prefer-predictable-action-arguments': 'error',
+      // XState rules
+      'xstate/spawn-usage': 0,
+      'xstate/state-names': ['warn', 'camelCase'],
+      'xstate/system-id': 'warn',
+    },
+  },
   {
     rules: {
       '@typescript-eslint/consistent-type-imports': [
